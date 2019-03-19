e84bf5150afd080dd610971ce1d89878df504005
shaow-mac-a0 <scozv@yandex.com>
2019-03-20 07:50:30 +0800

    fixed the implametation of kmp

--- lang/es6/src/text/kmp.js
+++ lang/es6/src/text/kmp.js
@@ -30,22 +30,49 @@ function kmp(str, pattern) {
 
   let result = [];
 
-  let j = 0;
-  for (let i = 0; i <= n - m; i++) {
-    while (j < m && str[i+j] === pattern[j]) { j++; }
-    if (j >= m) {
-      // matched on index at i
-      result.push(i);
-      j = 0;
-    } else if (j > 1) {
-      // not fully matched
+  // str[i] and pattern[j]
+  let i = 0, j = 0;
+  while (i < n) {
+    if (str[i] === pattern[j]) {
+      if (j === m - 1) {
+        // full matched, push the first index in `str` of match
+        result.push(i - j);
+        // move to next of i to start new search
+        i = i - j + 1;
+        j = 0;
+      } else {
+        // partial matched
+        i++;
+        j++;
+      }
+    } else if (j > 0) {
+      // unmatched
       let lengthOfPatternShift = j - next[j - 1];
       j = j - (lengthOfPatternShift);
-      i--;
     } else {
+      i++;
       j = 0;
     }
   }
+
+  // we start to compare str[i + q] and pattern[j + q];
+  // if match then q++
+  // let j = 0, q = 0;
+  // for (let i = 0; i <= n - m; i++) {
+  //   while (j < m && str[i+q] === pattern[j+q]) { q++; }
+  //   if (j+q >= m) {
+  //     // matched on index at i
+  //     result.push(i);
+  //     j = 0;
+  //   } else if (j > 1) {
+  //     // not fully matched
+  //     let lengthOfPatternShift = j - next[j - 1];
+  //     j = j - (lengthOfPatternShift);
+  //     i--;
+  //   } else {
+  //     j = 0;
+  //   }
+  // }
 
   return result;
 }
